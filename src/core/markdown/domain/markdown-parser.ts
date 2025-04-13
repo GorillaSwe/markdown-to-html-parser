e4289f05b1c904c412bc5e6e42e6cf6daf011385
GorillaSwe <52732673+GorillaSwe@users.noreply.github.com>
2025-04-13 14:27:18 +0900

    Parser のデフォルト変換を変更する (#13)

--- src/core/markdown/domain/markdown-parser.ts
+++ src/core/markdown/domain/markdown-parser.ts
@@ -4,6 +4,9 @@ import type { MarkdownNode } from "./markdown-node";
 @Injectable()
 export class MarkdownParser {
   parse(line: string): MarkdownNode {
+    if (line.trim() === "") {
+      return { type: "unknown", content: "" };
+    }
     if (line.startsWith("# ")) {
       return {
         type: "heading",
@@ -11,9 +14,6 @@ export class MarkdownParser {
         content: line.slice(2).trim(),
       };
     }
-    return {
-      type: "unknown",
-      content: line,
-    };
+    return { type: "paragraph", content: line.trim() };
   }
 }
